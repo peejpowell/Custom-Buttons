692ac50f38be2f75bc5d3a880d85dcc208d98231
Philip Powell <peejpowell@gmail.com>
2018-07-30 00:39:33 +0630

    Initial Commit

---
+++ Custom Buttons/Custom_Buttons.h
@@ -0,0 +1,19 @@
+//
+//  Custom_Buttons.h
+//  Custom Buttons
+//
+//  Created by Philip Powell on 30/07/2018.
+//  Copyright © 2018 Philip Powell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Custom_Buttons.
+FOUNDATION_EXPORT double Custom_ButtonsVersionNumber;
+
+//! Project version string for Custom_Buttons.
+FOUNDATION_EXPORT const unsigned char Custom_ButtonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Custom_Buttons/PublicHeader.h>
+
+
